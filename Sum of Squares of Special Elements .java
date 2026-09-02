505f3aeb9ef494e6bce72de02b5fb1c3bf8c518a
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-09-02 13:47:43 +0530

    Implement sumOfSquares method in Solution class

---
+++ Sum of Squares of Special Elements .java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfSquares(int[] nums) 
+    {
+        int sum = 0;
+        for(int i = 1; i <= nums.length; i++)
+        {
+        
+            if(nums.length%i==0)
+            {
+            int squre = nums[i-1] * nums[i-1];
+            sum += squre ; 
+            }
+        }
+        return sum;
+    }
+}
