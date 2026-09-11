91ecdfd18321211b1bba9a251ccf7b509fce7f76
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-09-11 22:41:18 +0530

    Update Bulls and Cows.java

--- Bulls and Cows.java
+++ Bulls and Cows.java
@@ -4,22 +4,19 @@ class Solution {
         int[] guesscount = new int[10];
         int bulls = 0;
         int cows = 0;
-
-        for (int i = 0; i < 10; i++) {
-            secretcount[i] = 0;
-            guesscount[i] = 0;
+        for (int i=0;i<10;i++) {
+            secretcount[i]=0;
+            guesscount[i]=0;
         }
-
-        for (int j = 0; j < secret.length(); j++) {
-            if (secret.charAt(j) == guess.charAt(j)) {
+        for (int j=0;j<secret.length();j++) {
+            if (secret.charAt(j)==guess.charAt(j)) {
                 bulls += 1;
             } else {
-                secretcount[secret.charAt(j) - '0'] += 1;
-                guesscount[guess.charAt(j) - '0'] += 1;
+                secretcount[secret.charAt(j)-'0']+=1;
+                guesscount[guess.charAt(j)-'0']+=1;
             }
         }
-
-        for (int p = 0; p < 10; p++) {
+        for (int p=0;p<10;p++) {
             cows += Math.min(secretcount[p], guesscount[p]);
         }
 
