42e9c52b11566299083b7bdd68efcb03075ea9ee
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-09-13 20:52:00 +0530

    Refactor digit sum and product calculation

--- Check Divisibility by Digit Sum and Product.java
+++ Check Divisibility by Digit Sum and Product.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean checkDivisibility(int n) {
-        int sum = 0;
-        int product = 1;
+        int sum=0;
+        int product=1;
         int x=n;
         while (x!=0) {
             int digit=x % 10;
-            sum=sum + digit;
+            sum=sum+digit;
             product=product*digit;
             x=x/10;
         }
