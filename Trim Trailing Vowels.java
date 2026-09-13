597cc3c4f195abae309ec9a1fb637cf0fcc128d5
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-09-13 20:58:50 +0530

    Fix formatting issues in trimTrailingVowels method

--- Trim Trailing Vowels.java
+++ Trim Trailing Vowels.java
@@ -1,7 +1,7 @@
 class Solution {
     public String trimTrailingVowels(String s) {
         int i=s.length()-1;
-        while (i >= 0 && "aeiou".indexOf(s.charAt(i))!=-1) {
+        while (i>=0 && "aeiou".indexOf(s.charAt(i))!=-1) {
             i--;
         }
         return s.substring(0,i+1);
