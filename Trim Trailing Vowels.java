637faf4acd974f3fffcb5e22913de11e0631264d
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-09-13 20:57:41 +0530

    Refactor trimTrailingVowels method for clarity

--- Trim Trailing Vowels.java
+++ Trim Trailing Vowels.java
@@ -1,12 +1,9 @@
 class Solution {
     public String trimTrailingVowels(String s) {
-
-        int i = s.length() - 1;
-
-        while (i >= 0 && "aeiou".indexOf(s.charAt(i)) != -1) {
+        int i=s.length()-1;
+        while (i >= 0 && "aeiou".indexOf(s.charAt(i))!=-1) {
             i--;
         }
-
-        return s.substring(0, i + 1);
+        return s.substring(0,i+1);
     }
 }
